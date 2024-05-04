a64c3570ae78d45d8db402858cbcc54bf25b7933
simsam8 <simvedaa@gmail.com>
2024-05-04 16:05:31 +0200

    move python with packages into lang

--- configuration.nix
+++ configuration.nix
@@ -96,14 +96,7 @@
     clang
     unzip
     python3
-    python311Packages.pip
-    python311Packages.virtualenv
-    python311Packages.pylint
-    python311Packages.flake8
-    python311Packages.black
-    python311Packages.isort
     nodejs_21
-    nodePackages.pyright
     lua-language-server
     nil
   ];
---
+++ user/lang/python.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ...}:
+
+{
+  
+  home.packages = with pkgs; [
+    python3
+    python311Packages.pip
+    python311Packages.virtualenv
+    python311Packages.pylint
+    python311Packages.flake8
+    python311Packages.black
+    python311Packages.isort
+    nodePackages.pyright
+  ];
+}
