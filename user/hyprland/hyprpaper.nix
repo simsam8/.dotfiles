7fd63c01160b1d2cb6b8c0f4fae4a5a0ef4913ca
simsam8 <simvedaa@gmail.com>
2024-05-09 12:39:46 +0200

    set splash to false

--- user/hyprland/hyprpaper.nix
+++ user/hyprland/hyprpaper.nix
@@ -4,5 +4,6 @@
   home.file.".config/hypr/hyprpaper.conf".text = ''
     preload = ~/.dotfiles/user/backgrounds/nixos.png
     wallpaper = , ~/.dotfiles/user/backgrounds/nixos.png
+    splash = false
   '';
 }
