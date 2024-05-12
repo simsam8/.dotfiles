0d906f2760579f48c3953ccfa3f06eff51803190
simsam8 <simvedaa@gmail.com>
2024-05-12 15:10:13 +0200

    switch background every 10min

--- user/hyprland/swww_randomize.sh
+++ user/hyprland/swww_randomize.sh
@@ -17,7 +17,7 @@ export SWWW_TRANSITION_FPS=60
 export SWWW_TRANSITION_STEP=2
 
 # This controls (in seconds) when to switch to the next image
-INTERVAL=300
+INTERVAL=600
 
 while true; do
 	find "$1" -type f \
