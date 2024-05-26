59cbc2186713ebf1b334346ed298b81f4911045b
simsam8 <simvedaa@gmail.com>
2024-05-26 15:43:06 +0200

    add config for gnome

--- system/wm/gnome.nix
+++ system/wm/gnome.nix
@@ -11,8 +11,10 @@
   # Configure X11
   services.xserver = {
     enable = true;
-    xkb.layout = "us";
-    xkb.variant = "dvp";
+    xkb = {
+      layout = "us, us, no, no";
+      variant = ",dvp,,dvorak";
+    };
     excludePackages = [ pkgs.xterm ];
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
