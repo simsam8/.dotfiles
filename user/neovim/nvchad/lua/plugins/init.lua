77ecd4c5619348a29810f84b2ea42cc85ce9bc03
simsam8 <simvedaa@gmail.com>
2024-06-11 21:03:47 +0200

    add markdown preview plugin

--- user/neovim/nvchad/lua/plugins/init.lua
+++ user/neovim/nvchad/lua/plugins/init.lua
@@ -57,6 +57,12 @@ return {
   --     }
   --   }
   -- },
+  {
+      "iamcco/markdown-preview.nvim",
+      cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+      ft = { "markdown" },
+      build = function() vim.fn["mkdp#util#install"]() end,
+  },
 
   {
     "nvim-tree/nvim-tree.lua",
