bc5bef4200d95c02375a3caa85f0393f621c2553
simsam8 <simvedaa@gmail.com>
2024-07-30 21:21:43 +0200

    use theme

--- user/firefox/default.nix
+++ user/firefox/default.nix
@@ -1,39 +1,10 @@
 { config, pkgs, inputs, systemSettings, userSettings, ... }:
 
 {
-  home.packages = with pkgs; [
-    firefox
-  ];
-  programs.firefox = {
-    enable = true;
-    package = pkgs.firefox;
-    profiles.simsam8 = {
-      id = 0;
-      name = userSettings.username;
-      isDefault = true;
-      extensions = with inputs.firefox-addons.packages."${systemSettings.system}"; [
-        bitwarden
-        darkreader
-        theme-nord-polar-night
-        sidebery
-        ublock-origin
-      ];
-      settings = {
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-        "browser.startup.page" = 3; # restore previous session
-        "layers.acceleration.force-enabled" = true;
-        "gfx.webrender.all" = true;
-        "gfx.webrender.enabled" = true;
-        "svg.context-properties.content.enabled" = true;
-        "layout.css.backdrop-filter.enabled" = true;
-        "extensions.autoDisableScopes" = 0;
-      };
-    };
-  };
+  imports = [ ./firefox.nix ];
 
   home.file.".mozilla/firefox/${userSettings.username}/chrome" = {
     source = ./blurredfox-master;
-    # source = ./nord-firefox-master;
     recursive = true;
     force = true;
   };
