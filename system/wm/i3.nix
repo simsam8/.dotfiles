b17a11bf872bfa565cee9a8a9b79f1e491925358
simsam8 <simvedaa@gmail.com>
2024-08-03 15:15:50 +0200

    remove xfce

--- system/wm/i3.nix
+++ system/wm/i3.nix
@@ -8,7 +8,7 @@
   ];
 
   services.displayManager = {
-    defaultSession = "xfce+i3";
+    defaultSession = "none+i3";
   };
 
   services.xserver = {
@@ -19,15 +19,6 @@
       options = "grp:alt_space_toggle";
     };
     excludePackages = [ pkgs.xterm ];
-    desktopManager = {
-      xterm.enable = false;
-      xfce = {
-        enable = true;
-        noDesktop = true;
-        enableXfwm = false;
-      };
-    };
-
     windowManager.i3 = {
       enable = true;
       extraPackages = with pkgs; [
