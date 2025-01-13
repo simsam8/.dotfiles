19c46ffd1df7aa1bc247a442e9ceff2ebd6c3c23
simsam8 <simvedaa@gmail.com>
2025-01-13 12:20:38 +0100

    move tree to left side

--- user/neovim/nvchad/lua/plugins/init.lua
+++ user/neovim/nvchad/lua/plugins/init.lua
@@ -103,7 +103,7 @@ return {
     opts = {
       view = {
         adaptive_size = false,
-        side = "right",
+        side = "left",
         width = 30,
         preserve_window_proportions = true,
       },
