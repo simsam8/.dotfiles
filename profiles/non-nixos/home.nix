13210e2edac7b585506604e640c83ad756b3f17b
simsam8 <simvedaa@gmail.com>
2025-06-04 23:03:37 +0200

    add obsidian and neofetch

--- profiles/non-nixos/home.nix
+++ profiles/non-nixos/home.nix
@@ -44,6 +44,8 @@
         "RobotoMono"
       ];
      })
+    obsidian
+    neofetch
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
