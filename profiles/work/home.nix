3f1b9a2b44920f08f7b7a4142714c6f86c5ef525
simsam8 <simvedaa@gmail.com>
2025-06-04 23:07:01 +0200

    add work profile

---
+++ profiles/work/home.nix
@@ -0,0 +1,85 @@
+{ lib, pkgs, inputs, userSettings, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = userSettings.username;
+  home.homeDirectory = "/home/" + userSettings.username;
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  fonts.fontconfig.enable = true;
+
+  imports = [
+    inputs.nix-colors.homeManagerModules.default
+    ../../user/alacritty/non_nixos.nix
+    ../../user/tmux
+    ../../user/neovim
+    ../../user/bash
+    ../../user/git.nix
+  ]
+  ++ # Set window manager if it exists
+  lib.optional (builtins.pathExists ../../user/${userSettings.wm}) ../../user/${userSettings.wm};
+
+  colorScheme = inputs.nix-colors.colorSchemes."${userSettings.theme}";
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    roboto-mono
+    (pkgs.nerdfonts.override {
+      fonts = [
+        "RobotoMono"
+      ];
+     })
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    "scripts".source = ../../scripts;
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/simsam/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = userSettings.editor;
+    SPAWNEDITOR = userSettings.spawnEditor;
+    TERM = userSettings.term;
+    # BROWSER = userSettings.browser;
+  };
+
+  news.display = "silent";
+}
