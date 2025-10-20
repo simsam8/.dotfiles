d167fae3f1eef435615a863e76f917315447cc38
Simon Vedaa <simvedaa@gmail.com>
2025-10-20 19:59:13 +0200

    disable dap

--- nvchad/.config/nvim/lua/mappings.lua
+++ nvchad/.config/nvim/lua/mappings.lua
@@ -12,20 +12,20 @@ map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "window down" })
 map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "window up" })
 
 -- DAP
-local dap = require "dap"
-vim.keymap.set("n", "<space>db", dap.toggle_breakpoint, { desc = "toggle breakpoint" })
-vim.keymap.set("n", "<space>dg", dap.run_to_cursor, { desc = "run to cursor" })
-
-vim.keymap.set("n", "<space>?", function()
-  require("dapui").eval(nil, { enter = true })
-end, { desc = "dap inspect value" })
-
-vim.keymap.set("n", "<F1>", dap.continue, { desc = "dap continue" })
-vim.keymap.set("n", "<F2>", dap.step_into, { desc = "dap step into" })
-vim.keymap.set("n", "<F3>", dap.step_over, { desc = "dap step over" })
-vim.keymap.set("n", "<F4>", dap.step_out, { desc = "dap step out" })
-vim.keymap.set("n", "<F5>", dap.step_back, { desc = "dap step back" })
-vim.keymap.set("n", "<F8>", dap.restart, { desc = "dap restart" })
+-- local dap = require "dap"
+-- vim.keymap.set("n", "<space>db", dap.toggle_breakpoint, { desc = "toggle breakpoint" })
+-- vim.keymap.set("n", "<space>dg", dap.run_to_cursor, { desc = "run to cursor" })
+--
+-- vim.keymap.set("n", "<space>?", function()
+--   require("dapui").eval(nil, { enter = true })
+-- end, { desc = "dap inspect value" })
+--
+-- vim.keymap.set("n", "<F1>", dap.continue, { desc = "dap continue" })
+-- vim.keymap.set("n", "<F2>", dap.step_into, { desc = "dap step into" })
+-- vim.keymap.set("n", "<F3>", dap.step_over, { desc = "dap step over" })
+-- vim.keymap.set("n", "<F4>", dap.step_out, { desc = "dap step out" })
+-- vim.keymap.set("n", "<F5>", dap.step_back, { desc = "dap step back" })
+-- vim.keymap.set("n", "<F8>", dap.restart, { desc = "dap restart" })
 
 -- gen.nvim
 vim.keymap.set({ "n", "v", "x" }, "<leader>]", ":Gen<CR>")
--- nvchad/.config/nvim/lua/plugins/init.lua
+++ nvchad/.config/nvim/lua/plugins/init.lua
@@ -112,43 +112,43 @@ return {
     },
   },
 
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-      "nvim-neotest/nvim-nio",
-      -- "theHamsta/nvim-dap-virtual-text",
-    },
-    config = function()
-      local dap = require "dap"
-      local dapui = require "dapui"
-      dapui.setup()
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
-    end,
-  },
-  {
-    "mfussenegger/nvim-dap-python",
-    ft = "python",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-      "rcarriga/nvim-dap-ui",
-    },
-    config = function()
-      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-      require("dap-python").setup(path)
-    end,
-  },
+  -- {
+  --   "rcarriga/nvim-dap-ui",
+  --   dependencies = {
+  --     "mfussenegger/nvim-dap",
+  --     "nvim-neotest/nvim-nio",
+  --     -- "theHamsta/nvim-dap-virtual-text",
+  --   },
+  --   config = function()
+  --     local dap = require "dap"
+  --     local dapui = require "dapui"
+  --     dapui.setup()
+  --     dap.listeners.before.attach.dapui_config = function()
+  --       dapui.open()
+  --     end
+  --     dap.listeners.before.launch.dapui_config = function()
+  --       dapui.open()
+  --     end
+  --     dap.listeners.before.event_terminated.dapui_config = function()
+  --       dapui.close()
+  --     end
+  --     dap.listeners.before.event_exited.dapui_config = function()
+  --       dapui.close()
+  --     end
+  --   end,
+  -- },
+  -- {
+  --   "mfussenegger/nvim-dap-python",
+  --   ft = "python",
+  --   dependencies = {
+  --     "mfussenegger/nvim-dap",
+  --     "rcarriga/nvim-dap-ui",
+  --   },
+  --   config = function()
+  --     local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+  --     require("dap-python").setup(path)
+  --   end,
+  -- },
   {
     "David-Kunz/gen.nvim",
     cmd = "Gen",
